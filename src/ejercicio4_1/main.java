94e9ac20183825fa11005ee6fcb7567236b4a327
LabA1_PC11 <cadizsalanovajarodzarian@gmail.com>
2023-10-26 10:51:29 -0400

    de nuevo

---
+++ src/ejercicio4_1/main.java
@@ -0,0 +1,11 @@
+package ejercicio4_1;
+
+public class main {
+    public static void main(String[] args) {
+        libro libro1=new libro("El Mar","Maria Del Barrio",250);
+        libro libro2=new libro("El Cielo","Manuel Gutierres",240);
+
+        libro1.imprimirlibro();
+        libro2.imprimirlibro();
+    }
+}
