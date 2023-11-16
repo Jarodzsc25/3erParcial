220283911191b271daf1962317c9edec463e0043
LabA1_PC11 <cadizsalanovajarodzarian@gmail.com>
2023-11-16 09:45:21 -0400

    nose que panso

--- src/ejercicio4_4/prueba.java
+++ src/ejercicio4_4/prueba.java
@@ -5,16 +5,15 @@ public class prueba {
 
         /*
         esta fraccion del codigo esta bien y no presenta problema
-        * */
+
         profesor profesor1  = new profesortitular();
         profesor1.imprimir();
-
-        /* buenas en primero lugar no me deja hacer nada me salta como un bucle y no puedo ejecutar el cofigo como tal.
-        profesor profesor11 = profesor1;
-        profesor profesor12 = profesor1;
-        profesor profesor2 = new (profesor)profesor1;
-
-        profesor2.imprimir();
         */
+
+        profesor profesor1 = new profesortitular();
+        profesor profesor2 = (profesor) profesor1;
+        profesor2.imprimir();
+
+
     }
 }
